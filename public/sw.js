fc6bba191b810d10eec39692abd532214c95e80d
ATMECS\Shubham.Tripathi <shubham.tripathi@atmecs.com>
2017-06-12 01:36:31 +0530

    Update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,12 @@
 const cacheList = ['./', './index.html', './icon1.png',
-                    './test.js', './error.html']
+                    './test.js', './error.html',
+                    'https://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.css',
+                    'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js',
+                    'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js',
+                    'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js',
+                    'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js',
+                    'https://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js',
+                    'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js']
 
 self.addEventListener('install', function (event) {
     console.log(event);
